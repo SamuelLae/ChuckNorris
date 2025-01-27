df2470ef6cfe84b7c12e52d73b041b56ba6d4980
samuel <samuel.laestander@edu.lycksele.se>
2025-01-27 13:04:57 +0100

    first commit

---
+++ ChuckNorris/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+
+
+class Program(){
+
+  static async void getJoke(){
+  using (HttpClient chuckNorris = new HttpClient()){
+      chuckNorris.BaseAddress = new Uri("https://api.chucknorris.io");
+       try{
+      HttpResponseMessage response = await chuckNorris.GetAsync("jokes/random");
+      response.EnsureSuccessStatusCode();
+      string responseBody = await response.Content.ReadAsStringAsync();
+      Console.WriteLine(responseBody);
+      } catch (HttpRequestException e){
+        Console.WriteLine(e.Message);
+      }
+       
+    };
+}
+  static void Main(){
+    getJoke();
+   
+  }
+}
